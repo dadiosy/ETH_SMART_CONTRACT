b3dfe2af3ccde87e1eb98f97a1ed9248d69b1560
roberto.song <roberto.song940726@gmail.com>
2024-06-13 21:28:29 -0700

    deploy contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.9",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/57222a0ec6f24c16b66be2fd05e0ecd6",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
